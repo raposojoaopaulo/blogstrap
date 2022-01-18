0e876579e47508ac585c9b40bcb00f21650125dc
raposojoaopaulo <raposojoaopaulo@gmail.com>
2022-01-18 17:55:15 -0300

    Adjust controller articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  validates :title, :presence => true, :length => {:minimum=>3, :maximum=>16}
+  validates :body, :presence => true, :length => {:minimum=>10, :maximum=>999}
 end
